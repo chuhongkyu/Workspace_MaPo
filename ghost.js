e3ab30be1b6f2da02c285edb31784748f5bf816a
Mr.chu <chuhongkyu@gmail.com>
2022-03-23 17:47:33 +0900

    2022_03_23

--- ghost.js
+++ ghost.js
@@ -1,8 +1,28 @@
+
+const Ghost = document.querySelector(".ghost");
+
 document.body.addEventListener("mousemove", function(e) {
-    console.log(e);
-    document.querySelector(".ghost").style.left = e.pageX + 'px';
-    document.querySelector(".ghost").style.top = e.pageY + 'px';
+    // console.log(e);
+    Ghost.style.left = e.pageX + 'px';
+    Ghost.style.top = e.pageY + 'px';
 });
 
 
+var appear = false;
 
+
+function hiddenGhost(){
+    appear = true;
+    Ghost.classList.add('hidden');
+    console.log("type");
+}
+
+function appearGhost(){
+    if(appear = true){
+        Ghost.classList.remove('hidden');
+        appear = false;
+    }
+}
+
+window.addEventListener("dblclick", hiddenGhost);
+window.addEventListener("click", appearGhost);
