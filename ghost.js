b7dbdb7705834a8d163ab563474e8a1586c9fc5d
Mr.chu <chuhongkyu@gmail.com>
2022-03-23 16:34:37 +0900

    update

--- ghost.js
+++ ghost.js
@@ -3,3 +3,6 @@ document.body.addEventListener("mousemove", function(e) {
     document.querySelector(".ghost").style.left = e.pageX + 'px';
     document.querySelector(".ghost").style.top = e.pageY + 'px';
 });
+
+
+
