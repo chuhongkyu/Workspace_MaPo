d957f12ef783b5a85059ce95da3e4bb0a133172a
Mr.chu <chuhongkyu@gmail.com>
2022-03-23 12:55:30 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const video = document.getElementById("#myVideo");
 const btn = document.getElementById("#myBtn");
 
+const LOCATION = document.getElementById("location_text");
+
 function myFunction() {
     if (video.paused) {
         video.play();
@@ -19,3 +21,25 @@ function self(){
 function newwindow(){
     window.open('https://www.naver.com');
 }
+
+function findLocation(){
+    navigator.geolocation? navigator.geolocation.getCurrentPosition(showPosition): LOCATION.innerHTML = "error";
+} // 이것이 삼항 연산자
+
+function showPosition(position){
+    LOCATION.innerHTML = "위도: " + position.coords.latitude +
+    "<br>경도: " + position.coords.longitude;
+}
+
+// function findLocation(){
+//     if('geolocation' in navigator){
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//         console.log('12312');
+        
+//     } else{
+//         LOCATION.innerHTML = "error";
+//     }
+// }
+
+//'geolocation' in navigator 두가지 방법
+///navigator.geolocation
