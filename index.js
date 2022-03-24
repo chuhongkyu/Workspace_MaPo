1eb60ed5403c2f2accd19964eba0ecd4b659bd0b
Mr.chu <chuhongkyu@gmail.com>
2022-03-24 09:24:56 +0900

    2022_03_24

--- index.js
+++ index.js
@@ -40,50 +40,52 @@ function findLocation(){
 
 const container = document.getElementById('map');
 
-var lat = 0;
-var long = 0;
+let lat = 0;
+let long = 0;
 
 function showPosition(position) {
-    LOCATION.innerHTML = "위도: " + position.coords.latitude +
-    "<br>경도: " + position.coords.longitude;
+    LOCATION.innerHTML = "위도: " + position.coords.latitude + "<br>경도: " +
+            position.coords.longitude;
 
-    var lat  = position.coords.latitude;
+    var lat = position.coords.latitude;
     var long = position.coords.longitude;
 
-    var container = document.getElementById('map'), 
-    mapOption = { 
-        center: new kakao.maps.LatLng(lat, long), 
-        level: 2 
-    };
-    var map = new kakao.maps.Map(container, mapOption); 
+    var container = document.getElementById('map'),
+        mapOption = {
+            center: new kakao
+                .maps
+                .LatLng(lat, long),
+            level: 2
+        };
+    var map = new kakao
+        .maps
+        .Map(container, mapOption);
 
-    
-   var markerPosition  = new kakao.maps.LatLng(lat, long); 
-   var marker = new kakao.maps.Marker({
-    position: markerPosition
-   });
+    var markerPosition = new kakao
+        .maps
+        .LatLng(lat, long);
+    var marker = new kakao
+        .maps
+        .Marker({position: markerPosition});
 
+    var iwContent = '<div style="padding:5px;">여기가 어디여~!</div>',
+        iwPosition = new kakao
+            .maps
+            .LatLng(lat + 0.0002, long),
+        iwRemoveable = true;
 
-   var iwContent = '<div style="padding:5px;">여기가 어디여~!</div>', 
-   iwPosition = new kakao.maps.LatLng(lat + 0.0002, long), 
-   iwRemoveable = true; 
+    var infowindow = new kakao
+        .maps
+        .InfoWindow(
+            {map: map, position: iwPosition, content: iwContent, removable: iwRemoveable}
+        );
 
-    var infowindow = new kakao.maps.InfoWindow({
-    map: map,
-    position : iwPosition, 
-    content : iwContent,
-    removable : iwRemoveable
-});
-
-
-
-marker.setMap(map);
-
+    marker.setMap(map);
+    console.log(long);
 
 }
 
-
-
+console.log(long, long);
 
 
 
