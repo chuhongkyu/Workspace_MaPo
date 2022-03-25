0de006014e7a125b4cc31cddeaa2da71e1acb012
Mr.chu <chuhongkyu@gmail.com>
2022-03-25 10:40:37 +0900

    update

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 const LOCATION = document.getElementById("location_text");
 
-
 function findLocation(){
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -19,6 +18,9 @@ let long = 0;
 const imageSrc = 'img/kakao.png';
 const imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
 const imageOption = {offset: new kakao.maps.Point(10, 0)};
+const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+const iwContent = '<div class="info_style" >백합아파트</div>';
 
 function showPosition(position) {
     LOCATION.innerHTML = "위도: " + position.coords.latitude + "<br>경도: " +
@@ -35,7 +37,6 @@ function showPosition(position) {
         };
     
 ////////////////////////////////////
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
     var map = new kakao.maps.Map(container, mapOption);
     var markerPosition = new kakao.maps.LatLng(lat, long);
     var marker = new kakao.maps.Marker({
@@ -44,30 +45,24 @@ function showPosition(position) {
     });
 
 //////////////////////////////////////
-    var iwContent = '<div class="info_style" >백합아파트</div>',
-        iwPosition = new kakao
-            .maps
-            .LatLng(lat, long),
+  
+    var iwPosition = new kakao.maps.LatLng(lat, long),
         iwRemoveable = true;
 
-    var infowindow = new kakao
-        .maps
+    var infowindow = new kakao.maps
         .InfoWindow(
             {map: map, position: iwPosition, content: iwContent, removable: iwRemoveable}
         );
 
     marker.setMap(map);
-    console.log(lat, long);
     infowindow.open(map, marker);
-
+    // console.log(lat, long);
 }
-
-
 
 
 function upMap(){
     LEVEL++
-    console.log(LEVEL);
+    // console.log(LEVEL);
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 
@@ -77,13 +72,11 @@ function downMap(){
         LEVEL = 0;
         alert("더 이상 확대할 수 없어요.")
     }
-    console.log(LEVEL);
+    // console.log(LEVEL);
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-
-
-console.log(lat, long);
+// console.log(lat, long);
 
 
 
