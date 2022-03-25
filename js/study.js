9111a226c551b6888a44f6d0f21bb2c4a5c2b1e9
Mr.chu <chuhongkyu@gmail.com>
2022-03-25 14:01:01 +0900

    Update 2022_032

--- js/study.js
+++ js/study.js
@@ -10,7 +10,7 @@
         },
         birdFlies2(key){
             if(key){
-                document.querySelector('[data-index="5"] .bird').style.transform =`translate(${window.innerWidth}px, ${-window.innerHeight * 7}px )`;
+                document.querySelector('[data-index="5"] .bird').style.transform =`translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px )`;
             }else{
                 document.querySelector('[data-index="5"] .bird').style.transform =`translateX(-100%)`;
             }
