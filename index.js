5559712e3614aa05e7075c1ef02622272bc9be74
Mr.chu <chuhongkyu@gmail.com>
2022-03-25 09:56:58 +0900

    update

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ let LEVEL = 2;
 let lat = 0;
 let long = 0;
 
+const imageSrc = 'img/building-solid.svg';
+const imageSize = new kakao.maps.Size(30, 30); // 마커이미지의 크기입니다
+const imageOption = {offset: new kakao.maps.Point(10, 0)};
+
 function showPosition(position) {
     LOCATION.innerHTML = "위도: " + position.coords.latitude + "<br>경도: " +
             position.coords.longitude;
@@ -52,23 +56,24 @@ function showPosition(position) {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
 
-        mapOption = {
+    var mapOption = {
             center: new kakao
                 .maps
                 .LatLng(lat, long),
             level: LEVEL
         };
-
+    
+////////////////////////////////////
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
     var map = new kakao.maps.Map(container, mapOption);
+    var markerPosition = new kakao.maps.LatLng(lat, long);
+    var marker = new kakao.maps.Marker({
+        position: markerPosition, 
+        image: markerImage // 마커이미지 설정 
+    });
 
-    var markerPosition = new kakao
-        .maps
-        .LatLng(lat, long);
-    var marker = new kakao
-        .maps
-        .Marker({position: markerPosition});
-
-    var iwContent = '<div style="padding:5px;">여기가 어디여~!</div>',
+//////////////////////////////////////
+    var iwContent = '<div style="padding:5px;">백합아파트</div>',
         iwPosition = new kakao
             .maps
             .LatLng(lat, long),
@@ -85,6 +90,8 @@ function showPosition(position) {
     infowindow.open(map, marker);
 
 }
+
+
 
 function upMap(){
     LEVEL++
