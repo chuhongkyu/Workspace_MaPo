e97a3b57490a4a3576ae9297c91bf52e94c23261
Mr.chu <chuhongkyu@gmail.com>
2022-03-28 15:06:40 +0900

    update

---
+++ js/test.js
@@ -0,0 +1,16 @@
+console.log("Hello.js")
+
+var num =3;
+console.log(num);
+
+let i =1;
+while (i<10){
+    console.log(i);
+    i = i+1;
+}
+
+
+let flag = false;
+do{
+    console.log("처음 실행");
+}while(flag === true);
