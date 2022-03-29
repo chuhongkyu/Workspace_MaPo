a065bd0f2ac5283947e5297be022819e1f9a609a
Mr.chu <chuhongkyu@gmail.com>
2022-03-29 15:29:00 +0900

    update 22_03_29

--- js/test.js
+++ js/test.js
@@ -2,16 +2,21 @@ const lottoBox = document.querySelector(".number_div");
 
 const maxnumber = [];
 function makeNumber(){
-  for(let i = 0; i< 7; i++){
-    maxnumber.push(Math.floor(Math.random()*100)+1);
-    // console.log(maxnumber);
-    console.log(maxnumber);
-    for(let c = 0; c< i; c++){
-      if (maxnumber[i] ===  maxnumber[c]) {
-          maxnumber.pop();
-          i--;  
-      }
-  }
+  while(maxnumber.length < 7){
+    let number = parseInt(Math.random()*100)+1;
+    if(maxnumber.indexOf(number) < 0){
+      maxnumber.push(number);
+    }
+  // for(let i = 0; i< 7; i++){
+  //   maxnumber.push(Math.floor(Math.random()*100)+1);
+  //   // console.log(maxnumber);
+  //   console.log(maxnumber);
+  //   for(let c = 0; c< i; c++){
+  //     if (maxnumber[i] ===  maxnumber[c]) {
+  //         maxnumber.pop();
+  //         i--;  
+  //     }
+  // }
   console.log(maxnumber);
   }
   
