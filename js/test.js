70a045f8b305d5380c7b9e90c449363b89731fa4
Mr.chu <chuhongkyu@gmail.com>
2022-03-29 12:41:13 +0900

    update

--- js/test.js
+++ js/test.js
@@ -1,16 +1,54 @@
-console.log("Hello.js")
+const LOTOPANEL = document.querySelector(".number_div");
 
-var num =3;
-console.log(num);
+const maxnumber = [];
+for(let i =1; i<=100; i +=1){
+    makeNumber.push(i);
+}
+console.log(maxnumber);
 
-let i =1;
-while (i<10){
-    console.log(i);
-    i = i+1;
+function makeNumber(){
+
 }
 
 
-let flag = false;
-do{
-    console.log("처음 실행");
-}while(flag === true);
+
+LOTOPANEL.addEventListener("onclick", makeNumber);
+
+
+
+
+// // let j = 6;
+
+// // for ( i = 1; i <= 9; i++){
+// //     {
+// //         console.log(i + "*" + j + "=" + i*j);
+// //     }
+// //  }
+
+// // ///7단
+
+// // let c = 7;
+
+// // for ( i = 1; i <= 9; i++){
+// //     {
+// //         console.log(i + "*" + c + "=" + i*c);
+// //     }
+// //  }
+
+// // ///8단
+
+// // let k = 8;
+// // for ( i = 1; i <= 9; i++){
+// //     {
+// //         console.log(i + "*" + k + "=" + i*k);
+// //     }
+// //  }
+
+
+
+//  for(let i= 6; i<=8; i++){
+//      for(let h = 1; h <= 9; h++){
+//         console.log(i + "*" + h + "=" + i*h);
+
+//      }
+//  }
