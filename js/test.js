a3decceafc18c6c64dd039f2f92e2ad049d51604
Mr.chu <chuhongkyu@gmail.com>
2022-03-30 13:01:34 +0900

    update

--- js/test.js
+++ js/test.js
@@ -1,31 +1,32 @@
 const lottoBox = document.querySelector(".number_div");
 
-const maxnumber = [];
 function makeNumber(){
+  const maxnumber = [];
   while(maxnumber.length < 7){
-    let number = parseInt(Math.random()*100)+1;
+    let number = parseInt(Math.random()*99)+1;
     if(maxnumber.indexOf(number) < 0){
       maxnumber.push(number);
     }
-  // for(let i = 0; i< 7; i++){
+  // console.log(maxnumber);
+  }
+  let numberText = document.createElement('p');
+  numberText.innerHTML ='로또 넘버 : '+ maxnumber ;
+  lottoBox.appendChild(numberText);
+  // maxnumber.length = 0;
+}
+// console.log(maxnumber);
+
+
+
+// for(let i = 0; i< 7; i++){
   //   maxnumber.push(Math.floor(Math.random()*100)+1);
   //   // console.log(maxnumber);
   //   console.log(maxnumber);
-  //   for(let c = 0; c< i; c++){
-  //     if (maxnumber[i] ===  maxnumber[c]) {
+  //   for(let j = 0; j< i; j++){
+  //     if (maxnumber[i] ===  maxnumber[j]) {
   //         maxnumber.pop();
   //         i--;  
   //     }
   // }
-  console.log(maxnumber);
-  }
-  
-  let numberText = document.createElement('p');
-  numberText.innerHTML ='로또 넘버 : '+ maxnumber ;
-  lottoBox.appendChild(numberText);
-  maxnumber.length = 0;
-}
-console.log(maxnumber);
-
 // LOTOPANEL.addEventListener("onclick", makeNumber);
 
