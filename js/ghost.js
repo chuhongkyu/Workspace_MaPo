84939c1fac97ed23331ad82dfc782e62163885ca
Mr.chu <chuhongkyu@gmail.com>
2022-07-12 14:11:04 +0900

    환경설정

--- js/ghost.js
+++ js/ghost.js
@@ -1,44 +1,40 @@
 const Ghost = document.querySelector(".ghost");
 
-document.body.addEventListener("mousemove", function(e) {
-    // console.log(e);
-    Ghost.style.left = e.pageX + 'px';
-    Ghost.style.top = e.pageY + 'px';
+document.body.addEventListener("mousemove", function (e) {
+  // console.log(e);
+  Ghost.style.left = e.pageX + "px";
+  Ghost.style.top = e.pageY + "px";
 });
 
 var appear = false;
 
-function hiddenGhost(){
-    appear = true;
-    Ghost.classList.add('hidden');
-    console.log("type");
+function hiddenGhost() {
+  appear = true;
+  Ghost.classList.add("hidden");
+  console.log("type");
 }
 
-function appearGhost(){
-    if(appear = true){
-        Ghost.classList.remove('hidden');
-        appear = false;
-    }
+function appearGhost() {
+  if ((appear = true)) {
+    Ghost.classList.remove("hidden");
+    appear = false;
+  }
 }
 
 window.addEventListener("dblclick", hiddenGhost);
 window.addEventListener("click", appearGhost);
 
-
 function createStar() {
-    const star= document.createElement("div");
-    star.classList.add('star');
-    star.style.marginLeft = randomPosition() + '%';
-    document.body.appendChild(star);
+  const star = document.createElement("div");
+  star.classList.add("star");
+  star.style.marginLeft = randomPosition() + "%";
+  document.getElementById("root").appendChild(star);
 }
 
 function randomPosition() {
-    return Math.floor(Math.random() * 100);
-    
-
+  return Math.floor(Math.random() * 100);
 }
 
-for (let i=0; i<500; i++) {
-    createStar();
+for (let i = 0; i < 500; i++) {
+  createStar();
 }
-
